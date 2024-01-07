340b2dbd143d58a37116bd836f58df774f7bf72c
Parminder Singh <engineer.parminder@gmail.com>
2024-01-07 17:35:38 +0800

    Take screenshot

--- main.cpp
+++ main.cpp
@@ -14,6 +14,24 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+void saveScreenshotToFile(std::string filename, int windowWidth, int windowHeight) {    
+    const int numberOfPixels = windowWidth * windowHeight * 3;
+    unsigned char pixels[numberOfPixels];
+
+    glPixelStorei(GL_PACK_ALIGNMENT, 1);
+    glReadBuffer(GL_FRONT);
+    glReadPixels(0, 0, windowWidth, windowHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, pixels);
+
+    FILE *outputFile = fopen(filename.c_str(), "w");
+    short header[] = {0, 2, 0, 0, 0, 0, (short) windowWidth, (short) windowHeight, 24};
+
+    fwrite(&header, sizeof(header), 1, outputFile);
+    fwrite(pixels, numberOfPixels, 1, outputFile);
+    fclose(outputFile);
+
+    printf("Finish writing to file.\n");
+}
+
 int main() {
     // glfw: initialize and configure
     // ------------------------------
@@ -57,6 +75,9 @@ int main() {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    
+    saveScreenshotToFile("test.tga", SCR_WIDTH, SCR_HEIGHT);
+    std::cout << "ScreenShot...." << std::endl;
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
